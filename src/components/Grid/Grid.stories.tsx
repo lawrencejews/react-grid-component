950927aeb3704887f25f0b20acaf0a99f672bb25
lawrencejews <lawrencejews1@gmail.com>
2021-07-18 17:35:33 +0900

    first commit

---
+++ src/components/Grid/Grid.stories.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Grid from "./Grid";
+import Box from '../Box/Box';
+import GridRuler from '../GridRuler/GridRuler';
+
+export default {
+    title: 'Grid-Components',
+    component: Grid,
+} as ComponentMeta<typeof Grid>;
+
+export const Basic: ComponentStory<typeof Grid> = (args) => (
+    <div style={{ position: 'relative' }}>
+
+        <Grid container spacing="sm" alignItems="center" style={{ height: "100%" }} {...args}>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Box>Box 1</Box>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Box>Box 2</Box>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Box>Box 3</Box>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Box>Box 4</Box>
+            </Grid>
+
+        </Grid>
+
+    </div>
+);
+
+export const withRuler: ComponentStory<typeof Grid> = (args) => (
+    <div style={{ position: 'relative' }}>
+        <GridRuler spacing="sm"></GridRuler>
+
+        <Grid container spacing="sm" alignItems="center" style={{ height: "100%" }} {...args}>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Box>Box 1</Box>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Box>Box 2</Box>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Box>Box 3</Box>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Box>Box 4</Box>
+            </Grid>
+
+        </Grid>
+
+    </div>
+);
+
+export const Interactive: ComponentStory<typeof Grid> = (args) => (
+    <div style={{ position: 'relative' }}>
+
+        <Grid container spacing="sm" alignItems="center" style={{ height: "100" }} >
+            <Grid item xs={1} >
+                <Box>Box 1</Box>
+            </Grid>
+
+            <Grid item xs={1} >
+                <Box>Box 2</Box>
+            </Grid>
+
+            <Grid item xs={1} >
+                <Box>Box 3</Box>
+            </Grid>
+
+            <Grid item xs={1} >
+                <Box>Box 4</Box>
+            </Grid>
+
+        </Grid>
+
+    </div>
+);
+
+
